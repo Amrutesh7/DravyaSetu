6e9bed5047f3ef2e19b9beba27b030cfc31ab83b
Narasimha A <nnarasimha1703@gmail.com>
2026-09-23 21:43:45 +0530

    initialized spring boot backend and tested health endpoints

---
+++ backend/src/main/java/com/dravya/setu/DravyaSetuApplication.java
@@ -0,0 +1,12 @@
+package com.dravya.setu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DravyaSetuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DravyaSetuApplication.class, args);
+    }
+}
